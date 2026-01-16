1893d5418cfcb8d483b096ee0848dcde443aa540
Tadam0 <triki.adam.lgf@gmail.com>
2026-01-16 10:05:41 +0000

    retour au menu principal après chaque interaction de l'utilisateur

--- src/main.cpp
+++ src/main.cpp
@@ -9,49 +9,51 @@ int main() {
   int choix;
   int trajet;
   int type;
-  cout << "****** Gestion d'une station Vélô ******" << endl << endl;
-  cout << "-----------------------------------------" << endl;
-  cout << "Vélos disponibles : " << velosDispos << endl;
-  cout << "Points d'attache disponibles: " << capacite - velosDispos << endl;
-  cout << "-----------------------------------------" << endl << endl;
-  cout << "1. Emprunter un vélô" << endl;
-  cout << "2. Restituer un vélô" << endl;
-  cout << "3. Estimer le coût de la location" << endl;
-  cout << "4. Quitter l'application" << endl << endl;
-  cin >> choix;
-  switch (choix) {
-		
-  case 1:
-    velosDispos = velosDispos - 1;
-    cout << "\n";
-    cout << "Vous empruntez un vélo.\n" << endl;
-		break;
+  do {
+    cout << "****** Gestion d'une station Vélô ******" << endl << endl;
+    cout << "-----------------------------------------" << endl;
+    cout << "Vélos disponibles : " << velosDispos << endl;
+    cout << "Points d'attache disponibles: " << capacite - velosDispos << endl;
+    cout << "-----------------------------------------" << endl << endl;
+    cout << "1. Emprunter un vélô" << endl;
+    cout << "2. Restituer un vélô" << endl;
+    cout << "3. Estimer le coût de la location" << endl;
+    cout << "4. Quitter l'application" << endl << endl;
+    cin >> choix;
+    switch (choix) {
 
-  case 2:
-    cout << "\n";
-    cout << "Vous restituez un vélo." << endl;
-		break;
-		
-  case 3:
-    cout << "Combien de temps durera votre trajet ? (en minutes)" << endl;
-    cin >> trajet;
-    cout << "Souhaitez vous emprunter un vélo :" << endl;
-    cout << "1. Mécanique" << endl;
-    cout << "2. Électrique" << endl;
-    cin >> type;
-    if (type == 1) {
+    case 1:
+      velosDispos = velosDispos - 1;
       cout << "\n";
-      cout << "Votre trajet coutera environ " << (trajet * coutHoraireMeca)/60 << " €" << endl;
-    }
-    if (type == 2) {
+      cout << "Vous empruntez un vélo.\n" << endl;
+      break;
+
+    case 2:
+			velosDispos = velosDispos + 1;
       cout << "\n";
-      cout << "Votre trajet coutera environ" << (trajet * coutHoraireElec)/60 << " €" << endl;
+      cout << "Vous restituez un vélo.\n" << endl;
+      break;
+
+    case 3:
+      cout << "Combien de temps durera votre trajet ? (en minutes)" << endl;
+      cin >> trajet;
+      cout << "Souhaitez vous emprunter un vélo : " << endl;
+      cout << "1. Mécanique" << endl;
+      cout << "2. Électrique" << endl;
+      cin >> type;
+      if (type == 1) {
+        cout << "\n";
+        cout << "Votre trajet coutera environ " << (trajet * coutHoraireMeca) / 60 << " €\n" << endl;
+      }
+      if (type == 2) {
+        cout << "\n";
+        cout << "Votre trajet coutera environ " << (trajet * coutHoraireElec) / 60 << " €\n" << endl;
+      }
+      break;
+
+    case 4:
+      cout << "Au revoir !" << endl;
+      return 0;
     }
-    break;
-		
-  case 4:
-    cout << "Au revoir !" << endl;
-    return 0;
-		
-  }
+  } while (choix != 4);
 }
